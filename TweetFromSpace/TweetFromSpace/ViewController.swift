76e593868526e59b1b82e1f9a85b090591e3d657
K <k@soracode-k.local>
2020-10-03 20:51:36 +0900

    k first commit

---
+++ TweetFromSpace/TweetFromSpace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TweetFromSpace
+//
+//  Created by K on 2020/10/03.
+//  Copyright © 2020 K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
---
+++ TweetFromSpace/TweetFromSpaceTests/TweetFromSpaceTests.swift
@@ -0,0 +1,34 @@
+//
+//  TweetFromSpaceTests.swift
+//  TweetFromSpaceTests
+//
+//  Created by K on 2020/10/03.
+//  Copyright © 2020 K. All rights reserved.
+//
+
+import XCTest
+@testable import TweetFromSpace
+
+class TweetFromSpaceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
